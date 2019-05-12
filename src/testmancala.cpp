f83cceb15129cce35ef277a13178c77237414b3c
seancarnahan <Vhellendoorn@ucdavis.edu>
2019-05-12 15:49:30 -0700

    added tests BasicMoveTest, ScoringMoveTest,DoubleMoveTest, and StealingMoveTest

--- src/testmancala.cpp
+++ src/testmancala.cpp
@@ -2,7 +2,7 @@
 #include "MancalaBoard.h"
 
 TEST(MancalaBoardTest, DefaultBoardTest){
-  /*
+
   CMancalaBoard newBoard;
 
   EXPECT_EQ(newBoard.PlayerTurn(), 0);
@@ -46,7 +46,7 @@ TEST(MancalaBoardTest, DefaultBoardTest){
       "\---------------------------/\n"
       "      1   2   3   4   5\n"
       "             PITS          P2\n");
-      */
+
 
 }
 
@@ -93,22 +93,229 @@ TEST(MancalaBoardTest, BasicMoveTest){
     // Needs to test that basic non-scoring moves are correct
     CMancalaBoard newBoard;
 
+    newBoard.Move(0, 0);
+    EXPECT_EQ(newBoard.ToString(),
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 5 | 5 | 5 | 5 | 0 |   |\n"
+        "| 0 |-------------------| 0 |\n"
+        "|   | 4 | 4 | 4 | 4 | 4 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+
+    newBoard.Move(1,0);
+    EXPECT_EQ(newBoard.ToString(),
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 5 | 5 | 5 | 5 | 0 |   |\n"
+        "| 0 |-------------------| 0 |\n"
+        "|   | 0 | 5 | 5 | 5 | 5 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+    newBoard.ResetBoard();
+
 }
 
 TEST(MancalaBoardTest, ScoringMoveTest){
     // Needs to test that basic scoring, non double moves, non steals are correct
+    CMancalaBoard newBoard;
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 0);
+    newBoard.Move(0, 1);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 0 | 4 | 4 | 4 | 4 |   |\n"
+        "| 1 |-------------------| 0 |\n"
+        "|   | 5 | 5 | 5 | 4 | 4 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 1);
+    newBoard.Move(1, 2);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 0 | 4 | 4 | 5 | 5 |   |\n"
+        "| 1 |-------------------| 1 |\n"
+        "|   | 5 | 5 | 0 | 5 | 5 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 0);
+    newBoard.Move(0, 2);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 1 | 5 | 0 | 5 | 5 |   |\n"
+        "| 2 |-------------------| 1 |\n"
+        "|   | 6 | 5 | 0 | 5 | 5 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 1);
+    newBoard.Move(1, 1);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 1 | 5 | 0 | 5 | 6 |   |\n"
+        "| 2 |-------------------| 2 |\n"
+        "|   | 6 | 0 | 1 | 6 | 6 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
 }
 
 TEST(MancalaBoardTest, DoubleMoveTest){
     // Needs to test that double move is correct
+    CMancalaBoard newBoard;
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 0);
+    newBoard.Move(0, 1);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 5 | 5 | 5 | 0 | 4 |   |\n"
+        "| 1 |-------------------| 0 |\n"
+        "|   | 4 | 4 | 4 | 4 | 4 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 0);
+    newBoard.Move(0, 2);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 6 | 6 | 0 | 0 | 4 |   |\n"
+        "| 2 |-------------------| 0 |\n"
+        "|   | 5 | 5 | 4 | 4 | 4 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 1);
+    newBoard.Move(1, 0);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 6 | 6 | 0 | 0 | 4 |   |\n"
+        "| 2 |-------------------| 1 |\n"
+        "|   | 0 | 6 | 5 | 5 | 5 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 1);
+    newBoard.Move(1, 4);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 6 | 7 | 1 | 1 | 5 |   |\n"
+        "| 2 |-------------------| 2 |\n"
+        "|   | 0 | 6 | 5 | 5 | 0 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 0);
+    newBoard.Move(0, 0);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 7 | 8 | 2 | 2 | 0 |   |\n"
+        "| 3 |-------------------| 2 |\n"
+        "|   | 0 | 6 | 5 | 5 | 0 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+
+    //checks: "If they reach their opponents store, the store is skipped"
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 0);
+    newBoard.Move(0, 3);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 8 | 0 | 2 | 2 | 1 |   |\n"
+        "| 4 |-------------------| 2 |\n"
+        "|   | 1 | 7 | 6 | 6 | 1 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
 }
 
 TEST(MancalaBoardTest, StealMoveTest){
     // Needs to test that stealing move works correctly
+    
+
+    int pits[] = {0,1,0,0,4,1,0,7,0,0};
+    int scores[] = {0,0};
+    CMancalaBoard newBoard(1, pits, scores);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 4 | 0 | 0 | 1 | 0 |   |\n"
+        "| 0 |-------------------| 0 |\n"
+        "|   | 1 | 0 | 7 | 0 | 0 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
+    EXPECT_EQ(newBoard.PlayerTurn(), 1);
+    newBoard.Move(1, 1);
+    EXPECT_EQ(newBoard.ToString(),
+
+        "P1          PITS\n"
+        "      5   4   3   2   1\n"
+        "/---------------------------\\n"
+        "|   | 4 | 0 | 0 | 0 | 0 |   |\n"
+        "| 8 |-------------------| 0 |\n"
+        "|   | 1 | 0 | 0 | 0 | 0 |   |\n"
+        "\---------------------------/\n"
+        "      1   2   3   4   5\n"
+        "             PITS          P2\n");
+
 }
 
 TEST(MancalaBoardTest, CantMoveTest){
     // Needs to test when player can't move correctly
+    //what is meant by: • If a player does not have any stones it their pits, they lose a turn
 }
 
 TEST(MancalaBoardTest, BadParametersTest){
