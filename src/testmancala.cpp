6f46c47a87727450f86d16a30c7e3d492df75e56
seancarnahan <Vhellendoorn@ucdavis.edu>
2019-05-13 13:12:47 -0700

    Added set and Reset test 9/9 tests complete; now adding to PAssing

--- src/testmancala.cpp
+++ src/testmancala.cpp
@@ -53,37 +53,50 @@ TEST(MancalaBoardTest, DefaultBoardTest){
 TEST(MancalaBoardTest, SetBoardTest){
     // Needs to test that setting up an initial board is correct
 
-    // CMancalaBoard gameBoard;
-    // int pits[] = {1,2,3,4,5,1,2,3,4,5};
-    // int scores[] = {5,5};
-    // CMancalaBoard gameBoard(0, pits, scores);
 
-    // for(int player = 0; player < MANCALA_PLAYERS; player++){
-    //     for (int pit = 0; pit < MANCALA_PITS; pit++){
-    //         EXPECT_EQ(gameBoard.PitStoneCount(player.pit), pit + 1);
-    //     }
-    //     EXPECT_EQ(gameBoard.PlayerScore(player), 5);
-    // }
-    // EXPECT_EQ(gameBoard.PlayerTurn(), 0);
-    // EXPECT_EQ(std::string(gameBoard), gameBoard.ToString()); //fix tostring
+    int pits[] = {1,2,3,4,5,1,2,3,4,5};
+    int scores[] = {5,5};
+    CMancalaBoard gameBoard(0, pits, scores);
+
+    for(int player = 0; player < MANCALA_PLAYERS; player++){
+        for (int pit = 0; pit < MANCALA_PIT_SLOTS; pit++){
+            EXPECT_EQ(gameBoard.PitStoneCount(player, pit), pit + 1);
+        }
+        EXPECT_EQ(gameBoard.PlayerScore(player), 5);
+    }
+    EXPECT_EQ(gameBoard.PlayerTurn(), 0);
+    EXPECT_EQ(std::string(gameBoard), gameBoard.ToString()); //fix tostring
 }
 
 TEST(MancalaBoardTest, ResetBoardTest){
     // Needs to test that resetting a board is correct
 
-    // CMancalaBoard gameBoardSet, gameBoardDefault;
-    // int pits[] = {1,2,3,4,5,1,2,3,4,5};
-    // int scores[] = {5,5};
-    // CMancalaBoard gameBoardSet(0, pits, scores);
+    int pits[] = {1,2,3,4,5,1,2,3,4,5};
+    int scores[] = {5,5};
+    CMancalaBoard gameBoardSet(0, pits, scores);
 
-    // for(int player = 0; player < MANCALA_PLAYERS; player++){
-    //     for (int pit = 0; pit < MANCALA_PITS; pit++){
-    //         EXPECT_EQ(gameBoardSet.PitStoneCount(player. pit), pit + 1);
-    //     }
-    //     EXPECT_EQ(gameBoardSet.PlayerScore(player), 5);
-    // }
-    // EXPECT_EQ(gameBoardSet.PlayerTurn(), 0);
-    // EXPECT_EQ(gameBoardSet.ResetBoard(), gameBoardDefault);
+    for(int player = 0; player < MANCALA_PLAYERS; player++){
+        for (int pit = 0; pit < MANCALA_PIT_SLOTS; pit++){
+            EXPECT_EQ(gameBoardSet.PitStoneCount(player, pit), pit + 1);
+        }
+        EXPECT_EQ(gameBoardSet.PlayerScore(player), 5);
+    }
+    EXPECT_EQ(gameBoardSet.PlayerTurn(), 0);
+
+    gameBoardSet.ResetBoard();
+
+    std::string result = "P1          PITS\n";
+    result += "      5   4   3   2   1\n";
+    result += R"(/---------------------------\)";
+    result += "\n";
+    result += "|   | 4 | 4 | 4 | 4 | 4 |   |\n";
+    result += "| 0 |-------------------| 0 |\n";
+    result += "|   | 4 | 4 | 4 | 4 | 4 |   |\n";
+    result += R"(\---------------------------/)";
+    result += "\n";
+    result += "      1   2   3   4   5\n";
+    result += "             PITS          P2\n";
+    EXPECT_EQ(gameBoardSet.ToString(), result);
 
 }
 
